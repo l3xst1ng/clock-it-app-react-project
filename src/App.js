5148aa89fec2905435245698fa94fca045d8a836
Muny1re1 <munyiriandy@gmail.com>
2024-05-09 22:33:31 +0300

    Added employee dashboard and its functionality and routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import LoginPage from './LoginPage';
+import Dashboard from './components/EmployeeDashboard';
+import LandingPage from './components/LandingPage.js';
 import './App.css';
+import {  useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [firstName, setFirstName] = useState("")
+
+    return (
+      <Router>
+        <Routes>
+          < Route path='/' element={<LandingPage/>}/>
+          <Route path='/LoginPage' element={<LoginPage setFirstName={setFirstName} firstName={firstName}/>}/>
+          <Route path='/DashBoard' element={<Dashboard firstName={firstName}/>}/>
+        </Routes>
+      </Router>
+    );
+
+  
+
 }
 
 export default App;
---
+++ src/components/EmployeeDashboard.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import AttendanceTable from "./AttendanceTable";
+import UserProfile from "./UserProfile";
+import './Dashboard.css'
+
+const EmployeeDashboard = ({firstName}) => {
+  const [attendanceData, setAttendanceData] = useState({
+    attendanceStatus: "",
+    date: "",
+    arrivalTime: ""
+  });
+  const [userName, setUserName] = useState("");
+  const [userRole, setUserRole] = useState("");
+
+  useEffect(() => {
+  
+
+    // Fetching user data from the JSON server
+    fetch("http://localhost:3000/user")
+      .then((response) => response.json())
+      .then((data) => {
+        const user = data.find(
+            user => user.firstName=== firstName
+        )
+        if (user){
+            setUserName(user.firstName + " " + user.lastName);
+         setUserRole(user.role);
+         const attenda = {...attendanceData, attendanceStatus: user.attendanceStatus, date: user.date, arrivalTime: user.arrivalTime}
+         console.log(attenda);
+         setAttendanceData(attenda)
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching user data:", error);
+      });
+  }, [firstName]);
+console.log(attendanceData);
+  return (
+    <div className="employee-dashboard">
+      <h1>Employee Dashboard</h1>
+      <UserProfile userName={userName} userRole={userRole} />
+      <AttendanceTable attendanceData={attendanceData} />
+    </div>
+  );
+};
+
+export default EmployeeDashboard;
