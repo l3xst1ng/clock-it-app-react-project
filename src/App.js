5ba7ad3272316bf76467b9127031f956ea4f85b7
l3xst1ng <howdy.martt@gmail.com>
2024-05-10 12:34:40 +0300

    Updated and optimized component routes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-import LoginPage from "./components/LoginPage";
-import EmployeeDashboard from "./components/EmployeeDashboard";
-import LandingPage from "./components/LandingPage";
-import About from "./components/About";
-import "./App.css";
-import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from './components/LoginPage';
+import EmployeeDashboard from './components/EmployeeDashboard.js';
+import LandingPage from './components/LandingPage.js';
+import About from './components/About.js';
+import WorkplaceGuidelines from './components/WorkplaceGuidelines.js';
+import Contact from './components/Contact.js';
+import './App.css';
+import {  useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [firstName, setFirstName] = useState("");
+  const [firstName, setFirstName] = useState("")
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route
-          path="/LoginPage"
-          element={
-            <LoginPage setFirstName={setFirstName} firstName={firstName} />
-          }
-        />
-        <Route
-          path="/EmployeeDashBoard"
-          element={<EmployeeDashboard firstName={firstName} />}
-              />
-              <Route
-                  path="/About"
-              element= {<About/>}/>
-      </Routes>
-    </Router>
-  );
+
+  
+
+
+    return (
+      <Router>
+        <Routes>
+          < Route path='/' element={<LandingPage/>}/>
+          <Route path='/LoginPage' element={<LoginPage setFirstName={setFirstName} firstName={firstName}/>}/>
+          <Route path='/DashBoard' element={<EmployeeDashboard firstName={firstName}/>}/>
+          <Route path='/About' element={<About/>}/>
+          <Route path='/WorkplaceGuidelines' element={<WorkplaceGuidelines/>}/>
+          <Route path='/Contacts' element={<Contact/>}/>
+        </Routes>
+      </Router>
+    );
+
+  
+
 }
 
 export default App;
+
