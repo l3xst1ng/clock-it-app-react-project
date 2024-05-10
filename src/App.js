2a576e8215a4f965915bfe1b7c67e0d82a88584f
Muny1re1 <munyiriandy@gmail.com>
2024-05-10 09:14:28 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,34 @@ import Dashboard from './components/EmployeeDashboard';
 import LandingPage from './components/LandingPage.js';
 import './App.css';
 
+import LoginPage from './LoginPage';
+import Dashboard from './Dashboard';
+import LandingPage from './LandingPage.js';
+import './App.css';
+import {  useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+function App() {
+  const [firstName, setFirstName] = useState("")
+
+
+  
+
+
+    return (
+      <Router>
+        <Routes>
+          < Route path='/' element={<LandingPage/>}/>
+          <Route path='/LoginPage' element={<LoginPage setFirstName={setFirstName} firstName={firstName}/>}/>
+          <Route path='/DashBoard' element={<Dashboard firstName={firstName}/>}/>
+        </Routes>
+      </Router>
+    );
+
+  
+
+}
+
+export default App;
 
 export default App;
